3dbc111f00db85d7134c80b36ffe60d5d5204719
Javier Pérez <jperez@smartup.es>
2021-03-04 07:57:59 +0100

    Add ListGAViewViewSet and permissions to views

--- src/conexiones/urls.py
+++ src/conexiones/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
 
-from .views import ConexionDBViewSet, ConexionAPIViewSet, GAViewViewSet
+from .views import ConexionDBViewSet, ConexionAPIViewSet, GAViewViewSet, ListGAViewViewSet
 
 router = routers.SimpleRouter()
 router.register(r'conexion-db', ConexionDBViewSet)
 router.register(r'conexion-api', ConexionAPIViewSet)
 router.register(r'ga-view', GAViewViewSet)
+router.register(r'list-ga-view', ListGAViewViewSet)
 urlpatterns = router.urls
--- src/conexiones/views.py
+++ src/conexiones/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from .models import ConexionDB, ConexionAPI, GAView
 from .serializers import ConexionDBSerializer, ConexionAPISerializer, GAViewSerializer
@@ -7,13 +8,21 @@ from .serializers import ConexionDBSerializer, ConexionAPISerializer, GAViewSeri
 class ConexionDBViewSet(viewsets.ModelViewSet):
     serializer_class = ConexionDBSerializer
     queryset = ConexionDB.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class ConexionAPIViewSet(viewsets.ModelViewSet):
     serializer_class = ConexionAPISerializer
     queryset = ConexionAPI.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class GAViewViewSet(viewsets.ModelViewSet):
+    serializer_class = GAViewSerializer
+    queryset = GAView.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class ListGAViewViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = GAViewSerializer
     queryset = GAView.objects.all()
