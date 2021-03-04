284483d60cc664ac476e258766bad0bb6b273a54
Javier Pérez <jperez@smartup.es>
2021-03-04 07:58:44 +0100

    Add drf_yasg and swagger urls

--- src/gaodcore/settings.py
+++ src/gaodcore/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'conexiones',
+    'drf_yasg',
     'rest_framework',
 ]
 
--- src/gaodcore/urls.py
+++ src/gaodcore/urls.py
@@ -14,9 +14,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='GA OD Core API',
+        default_version='v1',
+        description='GA OD Core API Swagger',
+        terms_of_service='https://opendata.aragon.es/terminos/',
+        contact=openapi.Contact(email='opendata@aragon.es'),
+        license=openapi.License(name='EUPL License'),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    re_path(r'^docs/swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^docs/swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('v1/conexiones/', include('conexiones.urls')),
 ]
