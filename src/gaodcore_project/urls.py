0f205d4fd25706f3c873f189060c647ef72b45e8
Emilio <choco@usal.es>
2021-08-02 12:58:35 +0200

    Fix name
    Name

--- src/gaodcore_project/urls.py
+++ src/gaodcore_project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from .configswagger import BothHttpAndHttpsSchemaGenerator
+from .configswagger import HttpsSchemaGenerator
 
 SchemaView = get_schema_view(
     openapi.Info(
@@ -30,7 +30,7 @@ SchemaView = get_schema_view(
         license=openapi.License(name='EUPL License'),
     ),
     public=False,
-    generator_class=BothHttpAndHttpsSchemaGenerator,
+    generator_class=HttpsSchemaGenerator,
 )
 
 urlpatterns = [
