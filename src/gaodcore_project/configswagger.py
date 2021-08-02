95562b2eaabe866ee665ab8ff9ddf0b27ee5f976
Emilio <choco@usal.es>
2021-08-02 12:58:35 +0200

    Fix name
    Name

--- src/gaodcore_project/configswagger.py
+++ src/gaodcore_project/configswagger.py
@@ -1,6 +1,6 @@
 from drf_yasg.generators import OpenAPISchemaGenerator
 
-class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
+class HttpsSchemaGenerator(OpenAPISchemaGenerator):
     def get_schema(self, request=None, public=False):
         schema = super().get_schema(request, public)
         schema.schemes = ["https"]
