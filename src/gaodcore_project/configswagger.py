0558f97d51b18469615a9c0c35d8694dd7ba9042
Guillermo Ferrer Bosque <22233599+guibos@users.noreply.github.com>
2021-09-14 15:06:30 +0200

    Autodetect https connection

--- src/gaodcore_project/configswagger.py
+++ src/gaodcore_project/configswagger.py
@@ -1,14 +1,8 @@
 from drf_yasg.generators import OpenAPISchemaGenerator
-# from collections import OrderedDict
 from drf_yasg import openapi
 
 
 class HttpsSchemaGenerator(OpenAPISchemaGenerator):
-    def get_schema(self, request=None, public=False):
-        schema = super().get_schema(request, public)
-        schema.schemes = ["https", "http"]
-        return schema
-
     def get_paths_object(self, paths):
         paths.move_to_end('/GA_OD_Core/views', last=False)
         return openapi.Paths(paths=paths)
