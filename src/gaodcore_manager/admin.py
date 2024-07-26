22fc9db9d0701ecb0a5290b4e7fb22613fa032dd
Oriol Garcia <62403724+oriolgaal@users.noreply.github.com>
2024-07-26 12:44:19 +0200

    Improve logging

--- src/gaodcore_manager/admin.py
+++ src/gaodcore_manager/admin.py
@@ -1,1 +1,28 @@
 # Register your models here.
+from django.contrib import admin
+
+from .models import ConnectorConfig, ResourceConfig, ResourceSizeConfig
+
+
+class ConnectorConfigAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'uri', 'enabled')
+    list_filter = ('enabled',)
+    search_fields = ('id', 'name', 'uri')
+
+
+class ResourceConfigAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'connector_config', 'object_location', 'object_location_schema', 'enabled')
+    list_filter = ('enabled',)
+    search_fields = (
+        'id', 'name', 'connector_config', 'object_location', 'object_location_schema')
+
+
+class ResourceSizeConfigAdmin(admin.ModelAdmin):
+    list_display = ('resource_id', 'registries', 'size')
+    search_fields = ('resource_id', 'registries', 'size')
+
+
+admin.site.register(ConnectorConfig, ConnectorConfigAdmin)
+admin.site.register(ResourceConfig, ResourceConfigAdmin)
+admin.site.register(ResourceSizeConfig, ResourceSizeConfigAdmin)
