6cfe7cc4e25d4011b03ad0b976a16e78006fec8e
opendata <opendata@aragon.es>
2024-08-27 11:38:10 +0200

    updated create_requests_view

--- scripts/create_requests_view.sh
+++ scripts/create_requests_view.sh
@@ -5,7 +5,7 @@ psql -h $POSTGRESQL_HOST -p $POSTGRESQL_PORT -d $POSTGRES_DB  -U $POSTGRES_USER 
 			drop materialized view if exists public.v_ip_count;
 			drop materialized view if exists public.v_resources_ip;
                         drop materialized view if exists public.v_requests_gaodcore;
-            CREATE materialized VIEW public.v_requests_gaodcore as SELECT datetime, url,
+            CREATE materialized VIEW public.v_requests_gaodcore as SELECT TO_CHAR(ear.datetime, 'YYYY-MM-DD HH:MM:SS') as datetime, url,
 				 case
 				 	when query_string!='' and query_string ~ 'resource_id=[0-9]*&' and (url = '/GA_OD_Core/preview' or url = '/GA_OD_Core/download')
 				 		then
@@ -41,6 +41,10 @@ psql -h $POSTGRESQL_HOST -p $POSTGRESQL_PORT -d $POSTGRES_DB  -U $POSTGRES_USER 
 		CREATE materialized VIEW public.v_resources_count as select resource_name, count(resource_name)  from v_requests_gaodcore vrg group by resource_name;
  CREATE materialized VIEW public.v_ip_count as select ip, count(ip) from v_requests_gaodcore vrg  group by ip  ;
  create materialized view public.v_resources_ip as select resource_name, count(distinct(ip)) as numero_llamadas from v_requests_gaodcore vrg group by resource_name;
- 
+
+GRANT SELECT on v_resources_ip TO gaodcore_read;
+GRANT SELECT on v_requests_gaodcore TO gaodcore_read;
+GRANT SELECT on v_ip_count TO gaodcore_read;
+GRANT SELECT on v_resources_count TO gaodcore_read; 
 
 "
