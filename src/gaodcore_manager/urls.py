cd237b60db8d243906be0963aaf9fc4c3f87b676
Oriol Garcia <62403724+oriolgaal@users.noreply.github.com>
2025-07-09 14:49:04 +0200

    Add login view and template for GA_OD_Core Manager

--- src/gaodcore_manager/urls.py
+++ src/gaodcore_manager/urls.py
@@ -4,7 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 from gaodcore_manager.views import (
     ConnectorConfigView, ResourceConfigView, ValidatorView,
-    ConnectorListView, ConnectorDetailView, ResourceListView, ResourceDetailView
+    ConnectorListView, ConnectorDetailView, ResourceListView, ResourceDetailView,
+    ManagerLoginView
 )
 
 router = routers.SimpleRouter()
@@ -21,6 +22,7 @@ urlpatterns = format_suffix_patterns([
 
 # Template-based views for web interface
 urlpatterns += [
+    path('login/', ManagerLoginView.as_view(), name='login'),
     path('views/connectors/', ConnectorListView.as_view(), name='connector-list'),
     path('views/connectors/<int:pk>/', ConnectorDetailView.as_view(), name='connector-detail'),
     path('views/resources/', ResourceListView.as_view(), name='resource-list'),
