4af00238a3f61cc8506c367efcbe693ee15298f5
Oriol Garcia <62403724+oriolgaal@users.noreply.github.com>
2025-07-17 13:28:03 +0200

    Add internationalization support for health monitoring templates and models

--- src/gaodcore_project/settings.py
+++ src/gaodcore_project/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -114,6 +115,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es-ES'
 
+LANGUAGES = [
+    ('es', 'Español'),
+    ('en', 'English'),
+]
+
 TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
@@ -121,6 +127,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
